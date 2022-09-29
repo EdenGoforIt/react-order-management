71553381d417938e0694b216752960f32d9d5a2e
EdenPark <edengoforit@gmail.com>
2022-09-30 08:37:18 +1300

    item component added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,15 +2,24 @@ import Modal from '../UI/Modal/Modal';
 import classes from './Cart.module.css';
 import React, { useContext } from 'react';
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem/CartItem';
 
 const Cart = (props) => {
 	const cartCtx = useContext(CartContext);
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-
+	const cartItemRemoveHandler = (id) => {};
+	const cartItemAddHandler = (item) => {};
 	const CartItems = (
 		<ul className={classes['cart-items']}>
 			{cartCtx.items.map((item) => (
-				<li>{item.name}</li>
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
 			))}
 		</ul>
 	);
