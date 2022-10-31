672b82218fcd7ca482726c3d9a75c988f1f95a6e
EdenPark <edengoforit@gmail.com>
2022-11-01 07:47:51 +1300

    loader and http error added in available meals

--- src/components/Meals/AvailableMeals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -6,12 +6,18 @@ import { useEffect, useState } from 'react';
 const AvailableMeals = () => {
 	const [meals, setMeals] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [httpError, setHttpError] = useState(); // undefined
+
 	useEffect(() => {
 		const fetchedMeals = async () => {
 			setIsLoading(true);
 			const response = await fetch(
 				'https://recipe-book-ffafc-default-rtdb.firebaseio.com/recipes.json'
 			);
+
+			if (!response.ok) {
+				throw new Error('Something went wrong');
+			}
 
 			const responseData = await response.json();
 			const loadedMeals = [];
@@ -25,14 +31,26 @@ const AvailableMeals = () => {
 			});
 
 			setMeals(loadedMeals);
-			setIsLoading(false);
+			setTimeout(() => {
+				setIsLoading(false);
+			}, 1000);
 		};
 
-		fetchedMeals();
+		fetchedMeals().catch((error) => {
+			setIsLoading(false);
+			setHttpError(error);
+		});
 	}, []);
 
 	if (isLoading) {
 		return <div className={classes.mealsLoading}></div>;
+	}
+	if (httpError) {
+		return (
+			<section className={classes.mealsError}>
+				<pre>error Occurred</pre>
+			</section>
+		);
 	}
 
 	const mealsList = meals.map((meal) => <MealItem key={meal.id} meal={meal} />);
